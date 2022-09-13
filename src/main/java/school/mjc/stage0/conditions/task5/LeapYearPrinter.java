5c2a1dce1536b491e63cb87cf77a5d6b81fa677e
Azim Shamuratov <a.shamuratov@atmos.uz>
2022-09-13 15:46:57 +0500

    Task #5. Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String result = year % 4 == 0 && !(year % 100 == 0) ? "leap" : year % 400 == 0 ? "leap" : "not leap";
+        switch (result) {
+            case "leap":
+                System.out.println("leap");
+                break;
+            case "not leap":
+                System.out.println("not leap");
+                break;
+        }
     }
 }
